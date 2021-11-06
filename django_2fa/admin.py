80560c9109890f61010668613cfbdf1625468fdc
Paul Bailey <paul@neutron.studio>
2021-11-06 10:08:04 -0500

    initial model

--- django_2fa/admin.py
+++ django_2fa/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from django_2fa.models import Device
+
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+  list_display = ('name', 'owner', 'created')
+  date_hierarchy = 'created'
+  search_fields = ('name', 'owner__username')
+
+  raw_id_fields = ('owner',)
--- django_2fa/models.py
+++ django_2fa/models.py
@@ -1,10 +1,23 @@
+from django.conf import settings
 from django.db import models
-from encrypted_fields.fields import EncryptedCharField
+
+from encrypted_fields.fields import EncryptedTextField
 
 
 class Device(models.Model):
+  DEVICE_TYPES = (
+    ('email', 'E-Mail'),
+    ('app', 'Authenicator App'),
+    ('hkey', 'Hardware Key'),
+  )
+
   name = models.CharField(max_length=64)
-  secret = EncryptedCharField(max_length=)
+  device_type = models.CharField(max_length=10, choices=DEVICE_TYPES)
+  secret = EncryptedTextField(blank=True, null=True)
+  counter = models.PositiveIntegerField(default=0)
+
+  owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+
   created = models.DateTimeField(auto_now_add=True)
 
   def __str__(self):
